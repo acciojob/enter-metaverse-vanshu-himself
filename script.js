bbdfb6fa52f0efafdd94acad9d5d8d9aa1900e56
vanshu-himself <124697864+vanshu-himself@users.noreply.github.com>
2023-05-20 21:56:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your JS code here. If required.
+const data=document.getquerySelector('#status').innerText;
+const btn=document.getElementById('enterBtn').addEventlistener('click',data="Entered Metaverse");
