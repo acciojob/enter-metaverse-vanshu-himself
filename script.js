7922f66f113bf06850cff91433677036eb877f59
vanshu-himself <124697864+vanshu-himself@users.noreply.github.com>
2023-05-21 13:20:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 //your JS code here. If required.
-function hii(){document.getElementById('status').innerHTML=""
-	document.getElementById('m').innerHTML="Entered Metaverse"};
+function hii(){
+	document.getElementById('status').innerHTML=""
+	const x=document.createElement('h1');
+	
+	x.innerHTML="Entered Metaverse";
+	document.getElementById('status').appendChild(x)};
